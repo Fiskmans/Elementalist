c74e04027199347304b862c645062f8a459bf8e0
Fiskmans - GLaDOS <mans@zingo.org>
2021-07-30 19:43:28 +0200

    Changed targetmod in build.gradle

---
+++ src/main/java/com/fiskmans/elementalist/Blocks/TestBlock/TestBlock.java
@@ -0,0 +1,10 @@
+package com.fiskmans.elementalist.Blocks.TestBlock;
+
+import net.minecraft.world.level.block.Block;
+
+public class TestBlock extends Block {
+
+    public TestBlock(Block.Properties properties) {
+        super(properties);
+    }
+}

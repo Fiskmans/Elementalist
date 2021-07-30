03b23411bbeb19a13bed0dd9115c144188c9e977
Fiskmans - GLaDOS <mans@zingo.org>
2021-07-30 23:01:51 +0200

    fixed blockstates

--- src/main/java/com/fiskmans/elementalist/Blocks/Blocks/ElementalPump.java
+++ src/main/java/com/fiskmans/elementalist/Blocks/Blocks/ElementalPump.java
@@ -2,10 +2,13 @@ package com.fiskmans.elementalist.Blocks.Blocks;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.common.ToolType;
 
 public class ElementalPump extends Block{
+
 
     public ElementalPump() {
         super(GetProperties());
